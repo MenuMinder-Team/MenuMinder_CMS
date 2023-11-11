d50ced6749d8da5a941db0ecb5d0258682d8647c
Anh Quan <quandang123654@gmail.com>
2023-11-11 08:30:48 +0700

    Update main page

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,9 +1,77 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { API } from '../constant/constant'
+import { catchError, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getAllDish() {
+    let headers = this.getHttpHeader();
+    return this.http.get(API.PRODUCT.END_POINT.PRODUCT, { headers }).pipe(
+      map((data: any) => {
+        if (data.meta.statusCode === API.PRODUCT.STATUS.GET_PRODUCT_SUCCESS) {
+          return data.data.productList
+        }
+        else {
+          return [];
+        }
+      }),
+      catchError((err) => {
+        throw new Error(err)
+      })
+    );
+  }
+  getCategory() {
+    return this.http.get(API.PRODUCT.END_POINT.CATEGORY).pipe(
+      map((data: any) => {
+        if (data.meta.statusCode === API.PRODUCT.STATUS.GET_PRODUCT_SUCCESS) {
+          return data.data.categoryList
+        }
+        else {
+          return [];
+        }
+      }),
+      catchError((err) => {
+        throw new Error(err)
+      })
+    );
+  }
+
+
+  addNewDish(formData: FormData) {
+    return this.http.post(API.PRODUCT.END_POINT.PRODUCT, formData, { headers: this.getHttpHeader() }).pipe(
+      map((data: any) => {
+        if (data.meta.statusCode === API.PRODUCT.STATUS.GET_PRODUCT_SUCCESS) {
+          return true;
+        }
+        else if (data.meta.statusCode === API.PRODUCT.STATUS.FAIL)
+          return false;
+        else {
+          return [];
+        }
+      }),
+      catchError((err) => {
+        throw new Error(err)
+      })
+    );
+  }
+
+
+  addNewCategory(name) {
+    return this.http.post(API.PRODUCT.END_POINT.CATEGORY, { name: name }, { headers: this.getHttpHeader() })
+  }
+
+
+
+
+
+  getHttpHeader(): HttpHeaders {
+    return new HttpHeaders({
+      'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem("jwtToken"))}`,
+    });
+  }
 }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { API_URL } from '../constant/constant'
+import { API } from '../constant/constant'
+import { catchError, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,85 @@ export class OrderService {
 
   constructor(private http: HttpClient) { }
 
+  getAllOrder() {
+    let headers = this.getHttpHeader();
+    return this.http.get(API.ORDER.END_POINT.ORDER, { headers }).pipe(
+      map((data: any) => {
+        if (data.meta.statusCode === API.ORDER.STATUS.GET_PRODUCT_SUCCESS) {
+          return data.data.productList
+        }
+        else {
+          return [];
+        }
+      }),
+      catchError((err) => {
+        throw new Error(err)
+      })
+    );
+  }
+
+  getAllOrderGroup() {
+    let headers = this.getHttpHeader();
+    return this.http.get(API.ORDER.END_POINT.ORDER_GROUP, { headers }).pipe(
+      map((data: any) => {
+        if (data.meta.statusCode === API.ORDER.STATUS.GET_PRODUCT_SUCCESS) {
+          return data.data.productList
+        }
+        else {
+          return [];
+        }
+      }),
+      catchError((err) => {
+        throw new Error(err)
+      })
+    );
+  }
+
+
+  addNewOrder(formData: FormData) {
+    return this.http.post(API.ORDER.END_POINT.ORDER, formData, { headers: this.getHttpHeader() }).pipe(
+      map((data: any) => {
+        if (data.meta.statusCode === API.ORDER.STATUS.GET_PRODUCT_SUCCESS) {
+          return true;
+        }
+        else if (data.meta.statusCode === API.ORDER.STATUS.FAIL)
+          return false;
+        else {
+          return [];
+        }
+      }),
+      catchError((err) => {
+        throw new Error(err)
+      })
+    );
+  }
+
+
+
+  checkOut(formData: FormData) {
+    return this.http.post(API.ORDER.END_POINT.ORDER, formData, { headers: this.getHttpHeader() }).pipe(
+      map((data: any) => {
+        if (data.meta.statusCode === API.ORDER.STATUS.GET_PRODUCT_SUCCESS) {
+          return true;
+        }
+        else if (data.meta.statusCode === API.ORDER.STATUS.FAIL)
+          return false;
+        else {
+          return [];
+        }
+      }),
+      catchError((err) => {
+        throw new Error(err)
+      })
+    );
+  }
+
+
+
+  getHttpHeader(): HttpHeaders {
+    return new HttpHeaders({
+      'Authorization': `Bearer ${JSON.parse(sessionStorage.getItem("jwtToken"))}`,
+    });
+  }
 
 }
