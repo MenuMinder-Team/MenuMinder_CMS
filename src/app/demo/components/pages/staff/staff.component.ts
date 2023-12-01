e10b7ef08891b1f5ef5a58e27556ec3b69b93ccd
TuongTran1301 <105165586+TuongTran1301@users.noreply.github.com>
2023-12-01 22:07:48 +0700

    update file staff.component.ts in staff

--- src/app/demo/components/pages/staff/staff.component.ts
+++ src/app/demo/components/pages/staff/staff.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./staff.component.scss']
 })
 export class StaffComponent {
-
+    genderOptions = [
+        { label: 'Male', value: 'male' },
+        { label: 'Female', value: 'female' },
+    ];
+    roleOptions = [
+        { label: 'Admin', value: 'admin' },
+        { label: 'Manager', value: 'manager' },
+        { label: 'Staff', value: 'staff' },
+    ];
 }
+
